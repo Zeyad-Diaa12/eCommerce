fe75dcc4ee3704f98de288b2ffffdebb9639df1f
Zeyad-Diaa12 <zeyaddiaa2@gmail.com>
2025-02-06 21:41:03 +0200

    product entity

---
+++ src/Services/Products/Products.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Products.API.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public decimal Price { get; set; }
+    public string Name { get; set; } = default;
+    public string ImageFile { get; set; } = default;
+    public string Description { get; set; } = default;
+    public List<string> Category { get; set; } = new();
+}
