71eec645847b02e33281a33ad9decf6d9554ee5f
Zeyad-Diaa12 <zeyaddiaa2@gmail.com>
2025-02-15 00:18:34 +0200

    products adjust

---
+++ src/Services/Products/Products.API/HostingExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Products.API;
+
+public static class HostingExtensions
+{
+    public static IServiceCollection AddCustomServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var assembly = typeof(Program).Assembly;
+        var connectionString = configuration.GetConnectionString("Products");
+
+        services.AddMediatR(
+            config =>
+            {
+                config.RegisterServicesFromAssembly(assembly);
+                config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+                config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+            }
+        );
+        
+        services.AddValidatorsFromAssembly(assembly);
+
+        services.AddCarter();
+
+        services.AddMarten(opts =>
+        {
+            opts.Connection(connectionString);
+        }).UseLightweightSessions();
+
+        if (services.BuildServiceProvider().GetService<IHostEnvironment>().IsDevelopment())
+        {
+            services.InitializeMartenWith<InitialSeedData>();
+        }
+
+        services.AddExceptionHandler<CustomExcpetionHandler>();
+
+        services.AddHealthChecks()
+            .AddNpgSql(connectionString);
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Products.API", Version = "v1" });
+        });
+
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            });
+        });
+
+        services.AddAuthentication();
+
+        services.AddAuthorization();
+
+        return services;
+    }
+}
--- src/Services/Products/Products.API/Program.cs
+++ src/Services/Products/Products.API/Program.cs
@@ -1,55 +1,9 @@
-using Microsoft.OpenApi.Models;
+using Products.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var assembly = typeof(Program).Assembly;
-var connectionString = builder.Configuration.GetConnectionString("Products");
-
-builder.Services.AddMediatR(config =>
-{
-    config.RegisterServicesFromAssembly(assembly);
-    config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
-    config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
-});
-builder.Services.AddValidatorsFromAssembly(assembly);
-
-builder.Services.AddCarter();
-
-builder.Services.AddMarten(opts =>
-{
-    opts.Connection(connectionString);
-}).UseLightweightSessions();
-
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services.InitializeMartenWith<InitialSeedData>();
-}
-
-builder.Services.AddExceptionHandler<CustomExcpetionHandler>();
-
-builder.Services.AddHealthChecks()
-    .AddNpgSql(connectionString);
-
-
-//Add Swagger
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Products.API", Version = "v1" });
-});
-
-builder.Services.AddCors(options => {
-    options.AddDefaultPolicy(builder => {
-        builder
-        .AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader();
-    });
-});
-
-builder.Services.AddAuthentication();
-builder.Services.AddAuthorization();
+builder.Services.AddCustomServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -57,7 +11,6 @@ var app = builder.Build();
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
-app.UseRouting();
 
 //CORS
 app.UseCors();
