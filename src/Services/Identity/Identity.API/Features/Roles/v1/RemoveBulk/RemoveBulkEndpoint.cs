392bb19b49d9e1be4f9b5d636d0dc297bebd1426
Zeyad-Diaa12 <zeyaddiaa2@gmail.com>
2025-02-18 22:15:17 +0200

    Added: Remove role from users in bulk

---
+++ src/Services/Identity/Identity.API/Features/Roles/v1/RemoveBulk/RemoveBulkEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using Identity.API.Features.Roles.v1.RemoveBulk;
+using Identity.Application.Handlers.RoleHandlers.RemoveBulk;
+
+namespace Identity.API.Features.Roles.v1.AssignBulk;
+
+public class RemoveBulkEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/api/v1/roles/remove/bulk",
+        async(RemoveBulkRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<RemoveBulkCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<RemoveBulkResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("RemoveRoleFromUsers")
+        .WithTags("RoleManager")
+        .Produces<RemoveBulkResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Remove Role from Users in Bulk")
+        .WithDescription("Remove Role from Users in Bulk")
+        .RequireAuthorization("RequireSuperAdminRole");
+    }
+}
---
+++ src/Services/Identity/Identity.API/Features/Roles/v1/RemoveBulk/RemoveBulkRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace Identity.API.Features.Roles.v1.RemoveBulk;
+
+public record RemoveBulkRequest(List<string> UserIds, string RoleName);
